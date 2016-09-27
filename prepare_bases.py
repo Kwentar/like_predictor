3332c65a12ef45a9dbc456714c61dfbfeaf07ba3
kwent <alekseev.yeskela@gmail.com>
2016-09-27 16:09:35 +0300

    added function for testing and removing previous version, fixed todo

--- prepare_bases.py
+++ prepare_bases.py
@@ -1,9 +1,10 @@
 import cv2
 import os
+from shutil import rmtree
 
 
 def write_weights_in_file(weights: dict, file_name: str):
-    with open(file_name, 'w+') as f:
+    with open(file_name, 'a+') as f:
         for key, value in weights.items():
             f.write(key.__str__() + ";" + value.__str__() + "\n")
 
@@ -18,20 +19,23 @@ def read_weights_from_file(file_name: str):
     return weights
 
 
-def resize_and_copy_images(dir_, dst_dir, weight_file_name='all_weights.txt', dir_for_images='images'):
-    current_index = 1
-    # Todo: get index
+def resize_and_copy_images(dir_, dst_dir_, current_index, weight_file_name='all_weights.txt', dir_for_images='images'):
     weights = read_weights_from_file(os.path.join(dir_, 'weights.txt'))
     image_names = os.listdir(dir_)
     image_exts = ['.jpg', '.png']
     image_names = list(filter(lambda x: os.path.splitext(x)[1].lower() in image_exts, image_names))
+    image_weights = dict()
     for image_name in image_names:
         image_weight = weights[image_name]
-        image = cv2.imread(image_name)
+        image = cv2.imread(os.path.join(dir_, image_name))
         new_image = cv2.resize(image, (256, 256))
         if new_image.shape:
-            # Todo save image in dir_for_images with name current_index.png and save weight of image in weight_file_name
-            pass
+            image_file_name = str(current_index) + '.png'
+            cv2.imwrite(os.path.join(os.path.join(dst_dir_, dir_for_images), image_file_name), new_image)
+            image_weights[image_file_name] = image_weight
+            current_index += 1
+    write_weights_in_file(image_weights, os.path.join(dst_dir_, weight_file_name))
+    return current_index
 
 
 def calc_weights(dir_):
@@ -50,14 +54,59 @@ def calc_weights(dir_):
             write_weights_in_file(likes, os.path.join(dir_, "weights.txt"))
 
 
-src_dir = '/media/kwent/A278648A78645F53/vk'
-dst_dir = '/home/kwent/bases/vk_likes'
-dirs = list(map(lambda x: os.path.join(src_dir, x), os.listdir(src_dir)))
-dirs = list(filter(os.path.isdir, dirs))
-count_person = len(dirs)
-index = 1
-for current_dir in dirs:
-    resize_and_copy_images(current_dir, dst_dir)
-    if index % 100 == 0:
-        print(index, '/', count_person, '|', round(index/count_person*100, 5), '%')
-    index += 1
+def test(dst_dir_, weight_file_name='all_weights.txt', dir_for_images='images'):
+    weights = read_weights_from_file(os.path.join(dst_dir_, weight_file_name))
+    image_names = os.listdir(os.path.join(dst_dir_, dir_for_images))
+    image_exts = ['.jpg', '.png']
+    image_names = list(filter(lambda x: os.path.splitext(x)[1].lower() in image_exts, image_names))
+    if len(image_names) != len(weights):
+        print('Alert: images count is {}, weights count is: {}'.format(len(image_names), len(weights)))
+        for image_name in image_names:
+            if image_name in weights.keys():
+                del weights[image_name]
+            else:
+                print('Alert: {} not in weights'.format(image_name))
+        for key in weights.keys():
+            print('Alert: {} weight without image'.format(key))
+        return
+    print('Test has been finished, all is ok')
+
+
+def clear_previous_dir(dir_):
+    for item in map(lambda x: os.path.join(dir_, x), os.listdir(dir_)):
+        if os.path.isdir(item):
+            rmtree(item)
+        else:
+            os.remove(item)
+
+
+def run():
+    src_dir = '/home/kwent/Bases/vk_test'
+    dst_dir = '/home/kwent/Bases/vk_likes'
+    dst_dir_for_images = 'images'
+    dst_weight_file_name = 'all_weights.txt'
+    dirs = list(map(lambda x: os.path.join(src_dir, x), os.listdir(src_dir)))
+    dirs = list(filter(os.path.isdir, dirs))
+    count_person = len(dirs)
+    index = 1
+    image_index = 1
+    print('Removing previous version...')
+    clear_previous_dir(dst_dir)
+    print('Starting conversion...')
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+    if not os.path.exists(os.path.join(dst_dir, dst_dir_for_images)):
+        os.makedirs(os.path.join(dst_dir, dst_dir_for_images))
+    for current_dir in dirs:
+        image_index = resize_and_copy_images(current_dir,
+                                             dst_dir,
+                                             image_index,
+                                             weight_file_name=dst_weight_file_name,
+                                             dir_for_images=dst_dir_for_images)
+        if index % 100 == 0:
+            print(index, '/', count_person, '|', round(index/count_person*100, 5), '%')
+        index += 1
+    print('Conversion has been finished!')
+    test(dst_dir_=dst_dir, weight_file_name=dst_weight_file_name, dir_for_images=dst_dir_for_images)
+
+run()
